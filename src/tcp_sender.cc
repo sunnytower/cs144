4e8413b2a34bd71ccc14971c035ee68beb47993c
sunnytower <ljxreal1@gmail.com>
2023-05-26 08:46:16 +0000

    save progress

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -7,31 +7,35 @@ using namespace std;
 
 /* TCPSender constructor (uses a random ISN if none given) */
 TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
-  : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) ), initial_RTO_ms_( initial_RTO_ms )
+  : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) )
+  , initial_RTO_ms_( initial_RTO_ms ), timer_ (initial_RTO_ms_)
 {}
 
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
   // Your code here.
-  return {};
+  return next_seqno_ - last_ackno_;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
   // Your code here.
-  return {};
+  return consecutive_retransmissions_;
 }
 
 optional<TCPSenderMessage> TCPSender::maybe_send()
 {
   // Your code here.
+  if ( !segments_out_.empty() ) {
+    const TCPSenderMessage msg = segments_out_.front();
+    segments_out_.pop_front();
+    return msg;
+  }
   return {};
 }
 
-void TCPSender::push( Reader& outbound_stream )
-{
-  // Your code here.
-  (void)outbound_stream;
+void TCPSender::push( Reader& outbound_stream ) { 
+
 }
 
 TCPSenderMessage TCPSender::send_empty_message() const
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -3,11 +3,68 @@
 #include "byte_stream.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
+#include <deque>
+#include <queue>
+
+class TCPTimer
+{
+private:
+  uint64_t initial_RTO_ms_;
+  uint64_t logical_RTO_ms_;
+  uint64_t current_RTO_ms_;
+  bool started_;
+
+public:
+  explicit TCPTimer( uint64_t initial_RTO_ms )
+    : initial_RTO_ms_( initial_RTO_ms ), logical_RTO_ms_( initial_RTO_ms ), current_RTO_ms_( 0 ), started_( true )
+  {}
+
+  bool isStart() const { return started_; }
+  uint64_t RTO() const { return logical_RTO_ms_; }
+  void doubleRTO() { logical_RTO_ms_ *= 2; }
+  void reset()
+  {
+    logical_RTO_ms_ = initial_RTO_ms_;
+    current_RTO_ms_ = 0;
+  }
+  void start()
+  {
+    started_ = true;
+    current_RTO_ms_ = 0;
+  }
+  void stop()
+  {
+    started_ = false;
+    current_RTO_ms_ = 0;
+  }
+  bool alarm( const size_t ms )
+  {
+    if ( started_ ) {
+      current_RTO_ms_ += ms;
+      if ( current_RTO_ms_ >= logical_RTO_ms_ ) {
+        current_RTO_ms_ = 0;
+        return true;
+      }
+    }
+    return false;
+  }
+};
 
 class TCPSender
 {
+private:
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
+  std::deque<TCPSenderMessage> segments_out_ {};
+  bool SYN { false };
+  bool FIN { false };
+  uint64_t consecutive_retransmissions_ { 0 };
+  std::queue<TCPReceiverMessage> outstanding_segments_ {};
+  TCPTimer timer_;
+  uint64_t next_seqno_ { 0 };
+  uint64_t last_ackno_ { 0 };
+  uint64_t last_window_size_ { 0 };
+  
 
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
