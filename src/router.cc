953308a35f52900d4f99455f0fd7235d885ad24f
sunnytower <ljxreal1@gmail.com>
2023-05-29 07:05:47 +0000

    save progress

--- src/router.cc
+++ src/router.cc
@@ -20,10 +20,40 @@ void Router::add_route( const uint32_t route_prefix,
        << static_cast<int>( prefix_length ) << " => " << ( next_hop.has_value() ? next_hop->ip() : "(direct)" )
        << " on interface " << interface_num << "\n";
 
-  (void)route_prefix;
-  (void)prefix_length;
-  (void)next_hop;
-  (void)interface_num;
+  route_table_.push_back( { route_prefix, prefix_length, next_hop, interface_num } );
 }
 
-void Router::route() {}
+void Router::route()
+{
+  /* use for longest prefix match */
+  sort( route_table_.begin(), route_table_.end(), []( const route_entry& a, const route_entry& b ) {
+    return a.prefix_length > b.prefix_length;
+  } );
+  /* query every interface */
+  for ( auto& interface_ : interfaces_ ) {
+    auto datagram = interface_.maybe_receive();
+    if ( datagram.has_value() ) {
+      /* drop the package */
+      if ( datagram->header.ttl == 0 || datagram->header.ttl == 1 ) {
+        continue;
+      }
+
+      /* match */
+      for ( const auto& entry : route_table_ ) {
+        /* match the prefix */
+        if ( ( datagram->header.dst & ( 0xFFFFFFFF << ( 32 - entry.prefix_length ) ) ) == entry.route_prefix ) {
+          /* match the next hop */
+          if ( entry.next_hop.has_value() ) {
+            /* send the package to the next hop */
+            interface_.send_datagram( datagram.value(), entry.next_hop.value() );
+          } else {
+            /* send the package to the destination */
+            interface( entry.interface_num )
+              .send_datagram( datagram.value(), Address::from_ipv4_numeric( datagram->header.dst ) );
+          }
+          break;
+        }
+      }
+    }
+  }
+}
--- src/router.hh
+++ src/router.hh
@@ -54,6 +54,14 @@ class Router
 {
   // The router's collection of network interfaces
   std::vector<AsyncNetworkInterface> interfaces_ {};
+  struct route_entry
+  {
+    uint32_t route_prefix;
+    uint8_t prefix_length;
+    std::optional<Address> next_hop;
+    size_t interface_num;
+  };
+  std::vector<route_entry> route_table_ {};
 
 public:
   // Add an interface to the router
